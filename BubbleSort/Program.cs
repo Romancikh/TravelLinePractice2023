19620ae63c4ff16aa9a4789d8bffeacb90c6d4fb
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-03 21:53:17 +0300

    Add method BubbleSort

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿class Program
+{
+    static List<T> BubbleSort<T>(List<T> list) where T : IComparable<T>
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = 0; j < list.Count; j++)
+            {
+                if (list[i].CompareTo(list[j]) < 0)
+                {
+                    (list[i], list[j]) = (list[j], list[i]);
+                }
+            }
+        }
+        return list;
+    }
+    static void Main(String[] args)
+    {
+        List<int> unsortedList = new()
+        {
+            5, 9, 1, 8, 2, 7, 4, 6, 3, 10, 5, 11, 4
+        };
+        List<int> sortedList = Program.BubbleSort(unsortedList);
+        sortedList.ForEach(item => Console.WriteLine(item));
+    }
+}
