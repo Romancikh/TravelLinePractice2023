a6085bca1b04337d8c11c52c884c1b319db5c36d
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-03 22:37:50 +0300

    Add data reading

--- PersonalInfo/Program.cs
+++ PersonalInfo/Program.cs
@@ -1,1 +1,18 @@
-﻿
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.Write("Фамилия: ");
+string FirstName = Console.ReadLine();
+Console.Write("Имя: ");
+string LastName = Console.ReadLine();
+Console.Write("Отчество: ");
+string MiddleName = Console.ReadLine();
+Console.Write("Возраст: ");
+if (!int.TryParse(Console.ReadLine(), out int age))
+{
+    Console.WriteLine("Не удалось получить возраст");
+}
+Console.Write("Email: ");
+string email = Console.ReadLine();
+Console.Write("GitHub: ");
+string github = Console.ReadLine();
