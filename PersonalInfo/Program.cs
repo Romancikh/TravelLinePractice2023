bc2bde0bec2174de343f3825a69d1ae23cf15e0a
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-04 15:14:30 +0300

    Split the main method

--- PersonalInfo/Program.cs
+++ PersonalInfo/Program.cs
@@ -7,35 +7,47 @@ class Program
     static string email = string.Empty;
     static string github = string.Empty;
     static int age = 0;
-    static void Main(string[] args)
+    static void ReadData()
     {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-        Console.OutputEncoding = Encoding.GetEncoding(1251);
-        Console.InputEncoding = Encoding.GetEncoding(1251);
+        Console.WriteLine("Введите данные");
         Console.Write("Фамилия: ");
-        lastName = Console.ReadLine();
+        lastName = Console.ReadLine()!;
         Console.Write("Имя: ");
-        firstName = Console.ReadLine();
+        firstName = Console.ReadLine()!;
         Console.Write("Отчество: ");
-        middleName = Console.ReadLine();
+        middleName = Console.ReadLine()!;
         Console.Write("Возраст: ");
+
         if (!int.TryParse(Console.ReadLine(), out age))
         {
             Console.WriteLine("Не удалось получить возраст");
             age = 0;
         }
+
         Console.Write("Email: ");
         email = Console.ReadLine();
         Console.Write("GitHub: ");
         github = Console.ReadLine();
-        Console.WriteLine();
+        Console.Clear();
+    }
+    static void WriteData()
+    {
         Console.WriteLine("Персональные данные");
         Console.WriteLine($"\tФамилия: {lastName}");
         Console.WriteLine($"\tИмя: {firstName}");
         Console.WriteLine($"\tОтчество: {middleName}");
         Console.WriteLine($"\tВозраст: {age}");
+        Console.WriteLine("Контакты");
         Console.WriteLine($"\tEmail: {email}");
         Console.WriteLine($"\tGitHub: {github}");
+    }
+    static void Main(string[] args)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Console.OutputEncoding = Encoding.GetEncoding(1251);
+        Console.InputEncoding = Encoding.GetEncoding(1251);
+        ReadData();
+        WriteData();
     }
 }
 
