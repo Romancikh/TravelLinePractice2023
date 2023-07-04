acf0cd3209cb121fc1c3f5f26a96a3c6d53d18e1
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-04 15:10:11 +0300

    Convert to class

--- PersonalInfo/Program.cs
+++ PersonalInfo/Program.cs
@@ -1,29 +1,35 @@
 using System.Text;
-
-Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-Console.OutputEncoding = Encoding.GetEncoding(1251);
-Console.InputEncoding = Encoding.GetEncoding(1251);
-Console.Write("Фамилия: ");
-string lastName = Console.ReadLine();
-Console.Write("Имя: ");
-string firstName = Console.ReadLine();
-Console.Write("Отчество: ");
-string middleName = Console.ReadLine();
-Console.Write("Возраст: ");
-if (!int.TryParse(Console.ReadLine(), out int age))
+class Program
 {
-    Console.WriteLine("Не удалось получить возраст");
-    age = 0;
+    static void Main(string[] args)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Console.OutputEncoding = Encoding.GetEncoding(1251);
+        Console.InputEncoding = Encoding.GetEncoding(1251);
+        Console.Write("Фамилия: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Имя: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Отчество: ");
+        string middleName = Console.ReadLine();
+        Console.Write("Возраст: ");
+        if (!int.TryParse(Console.ReadLine(), out int age))
+        {
+            Console.WriteLine("Не удалось получить возраст");
+            age = 0;
+        }
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+        Console.Write("GitHub: ");
+        string github = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("Персональные данные");
+        Console.WriteLine($"\tФамилия: {lastName}");
+        Console.WriteLine($"\tИмя: {firstName}");
+        Console.WriteLine($"\tОтчество: {middleName}");
+        Console.WriteLine($"\tВозраст: {age}");
+        Console.WriteLine($"\tEmail: {email}");
+        Console.WriteLine($"\tGitHub: {github}");
+    }
 }
-Console.Write("Email: ");
-string email = Console.ReadLine();
-Console.Write("GitHub: ");
-string github = Console.ReadLine();
-Console.WriteLine();
-Console.WriteLine("Персональные данные");
-Console.WriteLine($"\tФамилия: {lastName}");
-Console.WriteLine($"\tИмя: {firstName}");
-Console.WriteLine($"\tОтчество: {middleName}");
-Console.WriteLine($"\tВозраст: {age}");
-Console.WriteLine($"\tEmail: {email}");
-Console.WriteLine($"\tGitHub: {github}");
+
