7436713903dc242b2575c55a4a609673917c69d7
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-06 11:20:23 +0300

    Add public class TriangleTests

---
+++ Shapes/TriangleTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace Shapes.Tests
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        public void CalculateArea_WithValidSideLengths_ReturnsCorrectArea()
+        {
+            // Arange
+            double side1 = 5;
+            double side2 = 6;
+            double side3 = 7;
+            Triangle triangle = new( side1, side2, side3 );
+
+            // Act
+            double area = triangle.CalculateArea();
+
+            // Assert
+            double semiperimeter = triangle.CalculatePerimeter() / 2;
+            double expectedArea = Math.Sqrt( semiperimeter * ( semiperimeter - side1 ) * ( semiperimeter - side2 ) * ( semiperimeter - side3 ) );
+            Assert.AreEqual( expectedArea, area );
+        }
+
+        [Test]
+        public void CalculatePerimeter_WithValidSideLengths_ReturnsCorrectPerimeter()
+        {
+            // Arange
+            double side1 = 5;
+            double side2 = 6;
+            double side3 = 7;
+            Triangle triangle = new( side1, side2, side3 );
+
+            // Act
+            double perimeter = triangle.CalculatePerimeter();
+
+            // Assert
+            double expectedPerimeter = side1 + side2 + side3;
+            Assert.AreEqual( expectedPerimeter, perimeter );
+        }
+    }
+}
