3e056f1db5ad5d6eefc7f2c5427eaeb1edd3be6b
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-06 09:15:42 +0300

    Add public class Circle

---
+++ Shapes/Circle.cs
@@ -0,0 +1,33 @@
+﻿namespace Shapes
+{
+    internal class Circle : IShape
+    {
+        private double _radius;
+
+        public double Radius
+        {
+            get { return _radius; }
+            set
+            {
+                if ( value <= 0 )
+                    throw new ArgumentException( "Side length must be a positive value." );
+                _radius = value;
+            }
+        }
+
+        public Circle( double radius )
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
