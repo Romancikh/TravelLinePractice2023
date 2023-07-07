5f5260b30633e7ad9ef3780643d92dea0358f6c1
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-07 18:51:49 +0300

    Fix #2, rewrite to "clean"

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,32 +1,34 @@
 ﻿class Program
 {
-    static List<T> BubbleSort<T>(List<T> list) where T : IComparable<T>
+    static List<T> BubbleSort<T>( List<T> unsortedList ) where T : IComparable<T>
     {
-        for (int i = 1; i < list.Count; i++)
+        List<T> sortedList = new( unsortedList );
+        for ( int i = 1; i < sortedList.Count; i++ )
         {
             bool swapped = false;
-            for (int j = 0; j < list.Count - i; j++)
+            for ( int j = 0; j < sortedList.Count - i; j++ )
             {
-                if (list[j + 1].CompareTo(list[j]) < 0)
+                if ( sortedList[ j + 1 ].CompareTo( sortedList[ j ] ) < 0 )
                 {
-                    (list[j], list[j + 1]) = (list[j + 1], list[j]);
+                    (sortedList[ j ], sortedList[ j + 1 ]) = (sortedList[ j + 1 ], sortedList[ j ]);
                     swapped = true;
                 }
             }
-            if (!swapped)
+            if ( !swapped )
             {
-                return list;
+                return sortedList;
             }
         }
-        return list;
+        return sortedList;
     }
-    static void Main(string[] args)
+    static void Main( string[] args )
     {
         List<int> unsortedList = new()
         {
             5, 9, 1, 8, 2, 7, 4, 6, 3, 10, 5, 11, 4
         };
-        List<int> sortedList = BubbleSort(unsortedList);
-        Console.WriteLine(string.Join(", ", sortedList ));
+        List<int> sortedList = BubbleSort( unsortedList );
+        Console.WriteLine( $"Unsorted list: {string.Join( ", ", unsortedList )}" );
+        Console.WriteLine( $"Sorted list: {string.Join( ", ", sortedList )}" );
     }
 }
