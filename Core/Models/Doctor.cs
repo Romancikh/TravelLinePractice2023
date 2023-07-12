40561acb67eb64b24e4572c9210b20d237706f55
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 14:02:33 +0300

    Add default values for string properties

--- Core/Models/Doctor.cs
+++ Core/Models/Doctor.cs
@@ -3,9 +3,9 @@
     public class Doctor
     {
         public int DoctorId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Specialty { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Specialty { get; set; } = string.Empty;
         public List<Reception> Receptions { get; set; } = new List<Reception>();
         public override string ToString()
         {
