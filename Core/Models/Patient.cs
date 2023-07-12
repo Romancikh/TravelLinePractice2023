696196bdb8ebb1002c91b0c474273389b248ed39
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 13:48:39 +0300

    Add a patient model

--- Core/Models/Patient.cs
+++ Core/Models/Patient.cs
@@ -2,5 +2,14 @@
 {
     public class Patient
     {
+        public int PatientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Illness { get; set; }
+        public List<Reception> Receptions { get; set; } = new List<Reception>();
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} - {Illness}";
+        }
     }
 }
