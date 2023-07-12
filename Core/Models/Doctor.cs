bfd060a361e73930d008c9473fcc38d71eda8715
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 13:45:38 +0300

    Add a doctor model

---
+++ Core/Models/Doctor.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Specialty { get; set; }
+        public List<Reception> Receptions { get; set; } = new List<Reception>();
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} - {Specialty}";
+        }
+    }
+}
