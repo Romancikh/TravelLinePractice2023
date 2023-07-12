899f3e495bc6bad88026b17a0b5bd3c2642ed177
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 17:41:46 +0300

    Add a context factory

---
+++ HospitalDbMigrations/ContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseProvider;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace HospitalDbMigrations
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext( string[] args )
+        {
+            string connectionString =
+                "Data Source=MilkyWay\\SQLEXPRESS;Initial Catalog=Books;Pooling=true;Integrated Security=SSPI";
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionBuilder.UseSqlServer( connectionString, assembly => assembly.MigrationsAssembly( "HospitalDbMigrations" ) );
+            return new ApplicationContext( optionBuilder.Options );
+        }
+    }
+}
---
+++ HospitalDbMigrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HospitalDbMigrations
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            new ContextFactory().CreateDbContext( args ).Database.Migrate();
+        }
+    }
+}
