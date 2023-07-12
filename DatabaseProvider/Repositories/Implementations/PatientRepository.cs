10b75db5e03e3b42bbd405e482cc91e05451b370
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 16:56:06 +0300

    Add a patient repository implementation

---
+++ DatabaseProvider/Repositories/Implementations/PatientRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using DatabaseProvider.Repositories.Abstractions;
+
+namespace DatabaseProvider.Repositories.Implementations
+{
+    public class PatientRepository : Repository<Patient>, IPatientRepository
+    {
+        public PatientRepository( ApplicationContext context ) : base( context ) { }
+        public List<Patient> GetAll()
+        {
+            return Entities.ToList();
+        }
+        public Patient? GetById( int id )
+        {
+            return Entities.Where( p => p.PatientId == id ).FirstOrDefault();
+        }
+    }
+}
