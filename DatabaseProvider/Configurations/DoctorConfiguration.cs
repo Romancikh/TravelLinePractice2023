30995062a4edb3bbb22d2774a44d4df204e8cd56
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 14:40:30 +0300

    Add a doctor configuration

---
+++ DatabaseProvider/Configurations/DoctorConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DatabaseProvider.Configurations
+{
+    public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+    {
+        public void Configure( EntityTypeBuilder<Doctor> builder )
+        {
+            builder.ToTable( "Doctor" ).HasKey( d => d.DoctorId );
+            builder.Property( d => d.FirstName )
+                .HasColumnName( "DoctorId" )
+                .IsRequired();
+            builder.Property( d => d.FirstName )
+                .HasColumnName( "FirstName" )
+                .HasMaxLength( 50 )
+                .IsRequired();
+            builder.Property( d => d.LastName )
+                .HasColumnName( "LastName" )
+                .HasMaxLength( 50 )
+                .IsRequired();
+            builder.Property( d => d.Specialty )
+                .HasColumnName( "Specialty" )
+                .HasMaxLength( 50 )
+                .IsRequired();
+        }
+    }
+}
