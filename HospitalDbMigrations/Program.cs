2a7c0fc4fd35ab79f480a51f1faa30ca67c4889a
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 18:04:51 +0300

    Make public

--- HospitalDbMigrations/Program.cs
+++ HospitalDbMigrations/Program.cs
@@ -2,9 +2,9 @@
 
 namespace HospitalDbMigrations
 {
-    internal class Program
+    public class Program
     {
-        static void Main( string[] args )
+        public static void Main( string[] args )
         {
             new ContextFactory().CreateDbContext( args ).Database.Migrate();
         }
