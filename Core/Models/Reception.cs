06f1c065b11702e4f9843d19cc2beb2ec992d40d
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 13:56:32 +0300

    Add a reception model

--- Core/Models/Reception.cs
+++ Core/Models/Reception.cs
@@ -2,5 +2,17 @@
 {
     public class Reception
     {
+        public int ReceptionId { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+        public int RoomNumber { get; set; }
+        public override string ToString()
+        {
+            if ( Doctor == null || Patient == null ) return $"{ReceptionId} - {RoomNumber}";
+            return $"{Doctor}, {Patient} - {RoomNumber}";
+        }
+
     }
 }
