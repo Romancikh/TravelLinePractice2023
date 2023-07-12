49b8e09961ab2a1a525a6744c35aed9e03b8f83b
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 15:55:41 +0300

    Add an application context

---
+++ DatabaseProvider/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseProvider.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseProvider
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly string _connectionString;
+        public ApplicationContext( DbContextOptions<ApplicationContext> options ) : base( options ) { }
+        public ApplicationContext( string connectionString )
+        {
+            _connectionString = connectionString;
+        }
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+        {
+            if ( _connectionString == null )
+            {
+                return;
+            }
+            optionsBuilder.UseSqlServer( _connectionString );
+        }
+        protected override void OnModelCreating( ModelBuilder modelBuilder )
+        {
+            modelBuilder.ApplyConfiguration( new DoctorConfiguration() );
+            modelBuilder.ApplyConfiguration( new PatientConfiguration() );
+            modelBuilder.ApplyConfiguration( new ReceptionConfiguration() );
+        }
+    }
+}
