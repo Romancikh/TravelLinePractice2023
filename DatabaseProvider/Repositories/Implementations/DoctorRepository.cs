dbb804666b3fedbed0910d6cf5dc93dae093885c
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-12 16:52:19 +0300

    Add a doctor repository implementation

---
+++ DatabaseProvider/Repositories/Implementations/DoctorRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using DatabaseProvider.Repositories.Abstractions;
+
+namespace DatabaseProvider.Repositories.Implementations
+{
+    public class DoctorRepository : Repository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository( ApplicationContext context ) : base( context ) { }
+        public List<Doctor> GetAll()
+        {
+            return Entities.ToList();
+        }
+        public Doctor? GetById( int id )
+        {
+            return Entities.Where( d => d.DoctorId == id ).FirstOrDefault();
+        }
+    }
+}
