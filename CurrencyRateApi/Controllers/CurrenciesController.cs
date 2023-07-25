98fe3dc7c0c15805545b20146bc43f438e18cef9
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-07-25 21:23:11 +0300

    Add project files.

---
+++ CurrencyRateApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CurrencyRateApi.Models.Currency;
+using CurrencyRateApi.Services;
+
+namespace CurrencyRateApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrenciesController : ControllerBase
+    {
+        private ICurrencyService _currencyService;
+
+        public CurrenciesController(ICurrencyService userService)
+        {
+            _currencyService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _currencyService.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("{code}")]
+        public IActionResult GetByCode(string code)
+        {
+            var user = _currencyService.GetByCode(code);
+            return Ok(user);
+        }
+
+        [HttpGet, Route("/prices")]
+        public IActionResult GetPriceChanges([FromQuery] GetPricesRequest model)
+        {
+            var result = _currencyService.GetPriceChanges(model);
+            return Ok(result);
+        }
+    }
+}
---
+++ CurrencyRateApi/Models/Currency/PriceChange.cs
@@ -0,0 +1,13 @@
+﻿namespace CurrencyRateApi.Models.Currencies
+{
+    public class PriceChange
+    {
+        public string PurchasedCurrencyCode { get; set; } = string.Empty;
+
+        public string PaymentCurrencyCode { get; set;} = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
