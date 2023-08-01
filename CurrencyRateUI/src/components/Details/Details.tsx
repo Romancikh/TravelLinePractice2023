5dc8244a9e23247c5cc8092afdf71afb0cb55add
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-08-02 00:25:50 +0300

    Add currencies details

--- CurrencyRateUI/src/components/Details/Details.tsx
+++ CurrencyRateUI/src/components/Details/Details.tsx
@@ -1,19 +1,31 @@
+import { useContext } from "react";
 import "./Details.css";
+import { SelectedCurrenciesContext } from "../../context/selectedCurrencies";
 
 type DetailsProps = {
   className?: string;
 };
 
 function Details({ className }: DetailsProps) {
+  const { payment, purchased } = useContext(SelectedCurrenciesContext);
+
   return (
     <div className={`details ${className}`}>
       <div className="currency-details details__currency-details">
-        <div className="currency-details__heading">Heading 1</div>
-        <div className="currency-details__description">Some description</div>
+        <div className="currency-details__heading">
+          {payment.name}, {payment.symbol}
+        </div>
+        <div className="currency-details__description">
+          {payment.description}
+        </div>
       </div>
       <div className="currency-details details__currency-details">
-        <div className="currency-details__heading">Heading 1</div>
-        <div className="currency-details__description">Some description</div>
+        <div className="currency-details__heading">
+          {purchased.name}, {purchased.symbol}
+        </div>
+        <div className="currency-details__description">
+          {purchased.description}
+        </div>
       </div>
     </div>
   );
